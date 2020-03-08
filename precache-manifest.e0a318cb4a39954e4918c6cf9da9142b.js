04538066ea894b77d9a71daf31517ef34f787a09
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2020-03-07 19:49:21 -0600

    Updates

---
+++ precache-manifest.e0a318cb4a39954e4918c6cf9da9142b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb9051ad5ebf4b0affd96cfde6ad1e7",
+    "url": "/lusalga/robofriends/index.html"
+  },
+  {
+    "revision": "90dbb8daeb6785485c65",
+    "url": "/lusalga/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c1bf3df85b3d0b4c90c6",
+    "url": "/lusalga/robofriends/static/css/main.11b52dec.chunk.css"
+  },
+  {
+    "revision": "90dbb8daeb6785485c65",
+    "url": "/lusalga/robofriends/static/js/2.b6de1e5d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lusalga/robofriends/static/js/2.b6de1e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1bf3df85b3d0b4c90c6",
+    "url": "/lusalga/robofriends/static/js/main.3c986d04.chunk.js"
+  },
+  {
+    "revision": "4357d4677e58b2ec62a8",
+    "url": "/lusalga/robofriends/static/js/runtime-main.6688d6a2.js"
+  },
+  {
+    "revision": "12bc61e3fa30efef40169b69a59275f2",
+    "url": "/lusalga/robofriends/static/media/LifeCraft_Font.12bc61e3.woff"
+  }
+]);
