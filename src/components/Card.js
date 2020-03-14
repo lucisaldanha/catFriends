3e3da066e701e63a3038778dd1c4255e0c09128c
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2020-03-13 22:15:15 -0500

    changed css background of cards and input search field

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
 	return (
-		<div className= 'tc bg-light-purple dib br3 pa3 ma3 dim bw2 shadow-5 pointer'>
+		<div className= 'tc coolbackground dib br3 pa3 ma3 dim bw2 shadow-5 pointer'>
 			<img alt='robots' src={`https://robohash.org/${props.id}?size=300x300&set=set4`} />
 			<div>
 				<h2>{props.name}</h2>
