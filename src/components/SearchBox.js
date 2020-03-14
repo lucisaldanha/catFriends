463efa4aa6d2859e6bd06e1ea620e4f852b225cb
lusalga <ladsaldanha@gmail.com>
2020-03-13 22:15:15 -0500

    changed css background of cards and input search field

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,7 +5,7 @@ const SearchBox = ( {searchChange} ) => {
 	return (
 		<div className= 'pa2 mw-1'>
 			<input 
-				className="pa3 ba b--light-purple br2 bg-light-purple near-white" 
+				className="pa3 ba  b--transparent coolbackground br2 near-white" 
 				type="search" 
 				placeholder='type name of cute kitty friend' 
 				onChange= {searchChange}
