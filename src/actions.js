5d77f484cfc185417087d723fe162d65881696e3
lusalga <ladsaldanha@gmail.com>
2020-03-16 11:51:33 -0500

    updated comments

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,10 @@
 // We add here all Actions that will be dispatch to change the state tree. 
 
-// first we create the Action setSearchField, that receives 'text' as input, the user input,
+import { CHANGE_SEARCH_FIELD } from './constants.js';
+
+// First created setSearchField Action, that receives 'text' as input, the user input,
 // returning an object with type(action type) and payload keys and values.
 export const setSearchField = (text) => ({
-    type: 'CHANGE_SEARCH_FIELD', // user input typing on search box     
+    type: CHANGE_SEARCH_FIELD, // user input typing on search box     
     payload: text     // type of data
-})
+});
