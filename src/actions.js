43cc5ccf051a96059bfc09e4fba0a09e3e9bd168
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2020-03-16 19:11:45 -0500

    updated comments

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,12 @@
-// We add here all Actions that will be dispatch to change the state tree. 
+// We add here all Actions functions that will be dispatch to change 
+// the state tree.
 
+// Importanting const CHANGE_SEARCH_FIELD constant.
 import { CHANGE_SEARCH_FIELD } from './constants.js';
 
-// First created setSearchField Action, that receives 'text' as input, the user input,
-// returning an object with type(action type) and payload keys and values.
+// First create setSearchField Action which receives 'text' as input
+// (user input), returning an object with type(action type) and payload
+// (keys).
 export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD, // user input typing on search box     
     payload: text     // type of data
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-// creating a file with all of our constants for Redux implementation
+// creating a file with all of our constants for Redux implementation.
 
 export const CHANGE_SEARCH_FIELD = 'CHANGE_SEARCH_FIELD';
