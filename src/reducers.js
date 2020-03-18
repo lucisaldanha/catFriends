8f9782daec8a972e4b4f8d1f56c4e35a261365b6
lusalga <ladsaldanha@gmail.com>
2020-03-18 01:09:38 -0500

    added comments and tested redux with console.log

--- src/reducers.js
+++ src/reducers.js
@@ -7,6 +7,7 @@ const initialState = {
 
 // After initial state added, a reducer (pure function) is defined.
 export const searchCats = (state= initialState, action={}) => {
+    // console.log(action.type);
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, {searchField: action.payload});
